555bcc57b8f0cc19e1a8e37317df96aab2092024
XHH520 <1757867083@qq.com>
2026-07-27 23:33:13 +0800

    文本修复

--- ModSupport/ModularAdjacencyBonus/Config.sql
+++ ModSupport/ModularAdjacencyBonus/Config.sql
@@ -5,4 +5,4 @@ update Players set LeaderAbilityDescription = 'LOC_TRAIT_LEADER_MAGNIFICENCES_DE
 update Players set CivilizationAbilityDescription = 'LOC_TRAIT_CIVILIZATION_SCOTTISH_ENLIGHTENMENT_DESCRIPTION_MAB' where CivilizationType = 'CIVILIZATION_SCOTLAND';
 
 -- 红胡子
-update Players set LeaderAbilityDescription = 'LOC_TRAIT_LEADER_HOLY_ROMAN_EMPEROR_DESCRIPTION_MAB' where CivilizationType = 'CIVILIZATION_GERMANY';
+update Players set LeaderAbilityDescription = 'LOC_TRAIT_LEADER_HOLY_ROMAN_EMPEROR_DESCRIPTION_MAB' where LeaderType = 'LEADER_BARBAROSSA';
